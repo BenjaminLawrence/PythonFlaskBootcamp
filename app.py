b177d9953bd14bedb192ad046569d1b9effa760c
Benjamin Lawrence <83344040+BenjaminLawrence@users.noreply.github.com>
2023-08-06 21:32:27 +0800

    Move and rename index route.

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask
 # Intialize flask object, __name__ is resolved to __main__
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "Welcome to the index"
+
 if __name__ == '__main__':
     app.run()
-
-@app.route("/")
-def home():
-    return "Hello, Flask!"
