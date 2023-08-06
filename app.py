dc3dd11d8b900039aafe02c603db4686023e2a55
Benjamin Lawrence <83344040+BenjaminLawrence@users.noreply.github.com>
2023-08-06 21:30:29 +0800

    Add run call.

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask
 # Intialize flask object, __name__ is resolved to __main__
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    app.run()
+
 @app.route("/")
 def home():
     return "Hello, Flask!"
